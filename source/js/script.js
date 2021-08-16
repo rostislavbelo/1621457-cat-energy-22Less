3f782b3f4c3c838db286c503749c4033a5eaa72b
Rostislav Belo <rabelousov@gmail.com>
2021-08-16 17:43:21 +0300

    делает слайдер с котами на всех брейкпоинтах

--- source/js/script.js
+++ source/js/script.js
@@ -14,5 +14,29 @@ hamburger.onclick = function () {
   mainNav.classList.toggle("main-nav__list--active")
 };
 
+const compSliderControl = document.querySelector(".progress-slider__input");
+const imageBefore = document.querySelector(".example__img-box--before")
+const imageAfter = document.querySelector(".example__img-box--after")
+const buttonBefore = document.querySelector(".progress-slider__button--start")
+const buttonAfter = document.querySelector(".progress-slider__button--end");
+
+
+compSliderControl.oninput = function () {
+  imageBefore.style.width = 100 - compSliderControl.value + "%";
+  imageAfter.style.width = compSliderControl.value + "%"
+};
+
+buttonBefore.onclick = function () {
+  imageBefore.style.width = "100%";
+  imageAfter.style.width = 0;
+  compSliderControl.value = 0;
+};
+
+buttonAfter.onclick = function () {
+  imageAfter.style.width = "100%";
+  imageBefore.style.width = 0;
+  compSliderControl.value = 100;
+};
+
 
 
